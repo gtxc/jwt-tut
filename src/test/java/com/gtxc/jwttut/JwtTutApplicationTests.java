db6386933bf724bc35ab2785c84a876e411329c1
gt <gurkanturkarslan@hotmail.com>
2022-03-23 14:56:10 +0300

    Initialize project using spring initializr

---
+++ src/test/java/com/gtxc/jwttut/JwtTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtxc.jwttut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
